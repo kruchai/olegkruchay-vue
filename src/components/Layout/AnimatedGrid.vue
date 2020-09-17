b72e69987e004e743130feb38fdc2c8f57dff785
Oleg Kruchay <o.kruchay@gmail.com>
2020-09-17 09:29:44 +0300

    v0.1: Adding the animated Grid layout. Creating a separate component.

---
+++ src/components/Layout/AnimatedGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="animated-grid-container">
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+    <div class="line"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AnimatedGrid'
+};
+</script>
