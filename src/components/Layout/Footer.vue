740f967bcb3f36bef26abc6b24d24de0795ed06f
Oleg Kruchay <o.kruchay@gmail.com>
2020-09-17 10:46:57 +0300

    v0.2: Adding the Header, Main and Footer components with a custom grid system.

---
+++ src/components/Layout/Footer.vue
@@ -0,0 +1,38 @@
+<template>
+  <footer class="footer">
+    <!--  Cell 1  -->
+    <div class="cell first">&copy; {{ copyright }}</div>
+
+    <!--  Cell 2  -->
+    <div class="cell second">
+      <div>Build on <a href="#">Vue</a>.</div>
+      <div>Hosted on <a href="#">Netlify</a>.</div>
+      <div>The code is open-source on <a href="#">Github</a>.</div>
+    </div>
+
+    <!--  Cell 3  -->
+    <div class="cell third">
+      <div class="social-networks">
+        <img alt="Oleg Kruchay logo" src="../../assets/insta.png" />
+        <img alt="Oleg Kruchay logo" src="../../assets/linkedin.png" />
+        <img alt="Oleg Kruchay logo" src="../../assets/facebook.png" />
+        <img alt="Oleg Kruchay logo" src="../../assets/drupal.png" />
+        <img alt="Oleg Kruchay logo" src="../../assets/codepen.png" />
+      </div>
+    </div>
+
+    <!--  Absolute section  -->
+    <div class="brand-name">
+      <img alt="Oleg Kruchay logo" src="../../assets/footer-logo-cropped.png" />
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  props: {
+    copyright: String
+  }
+};
+</script>
---
+++ src/components/Layout/Main.vue
@@ -0,0 +1,44 @@
+<template>
+  <main class="main">
+    <!--  Cell 1  -->
+    <div class="cell first">
+      <div class="hello">
+        {{ greeting }}
+      </div>
+      <div class="who-i-am">
+        {{ message }}
+      </div>
+
+      <div class="cv-container">
+        <div class="download-cv">
+          <button class="cta">
+            <span class="circle" aria-hidden="true">
+              <span class="icon arrow"></span>
+            </span>
+            <span class="button-text">
+              Download my CV
+            </span>
+          </button>
+        </div>
+        <div class="message-me">
+          <button class="cta">
+            <span class="circle" aria-hidden="true">
+              <span class="icon arrow"></span>
+            </span>
+            <span class="button-text">Message me</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  props: {
+    greeting: String,
+    message: String
+  }
+};
+</script>
