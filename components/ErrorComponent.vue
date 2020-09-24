a801a5a1aa144f6558cb59b7133331d1ed4704fc
Oleh Kruchai <okruchay@gmail.com>
2020-09-24 03:10:37 +0300

    v0.6: Adding the ErrorComponent with the special markup and conditions. Updating error layout.

---
+++ components/ErrorComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <main class="v-section--main">
+    <div class="v-cell first">
+      <div v-if="statusCode === 404">
+        <span class="hello">{{ errors.notFound.heading }} 🗿</span>
+        <h1>
+          <span class="who-i-am">{{ errors.notFound.description }}</span>
+        </h1>
+      </div>
+      <div v-else>
+        <span class="hello">{{ heading }} 🐣</span>
+        <h1>
+          <span class="who-i-am">{{ description }}</span>
+        </h1>
+      </div>
+      <BackToHome />
+    </div>
+  </main>
+</template>
+
+<script>
+import {errors} from '@/static/data/data.json'
+import BackToHome from '@/components/BackToHome'
+
+export default {
+  name: 'ErrorComponent',
+  components: {BackToHome},
+  props: ['statusCode', 'heading', 'description'],
+  data() {
+    return {
+      errors,
+    }
+  },
+}
+</script>
