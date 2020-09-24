0f9483da009ef69a24e30bff4e198ca824fe9846
Oleh Kruchai <okruchay@gmail.com>
2020-09-24 03:11:45 +0300

    v0.6: Creating the simple BackToHome component with SSR link.

---
+++ components/BackToHome.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="back-to-home">
+    <NuxtLink exact-active-class="v-link active" class="v-link hello" to="/">{{ '🏡 Back to Home page' }}</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackToHome',
+}
+</script>
