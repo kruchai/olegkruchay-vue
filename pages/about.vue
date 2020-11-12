336b8649eed09e72fe9d185ead0f0753838b4c4d
Oleg Kruchay <okruchay@gmail.com>
2020-11-12 09:52:30 +0200

    v0.7.6: Adding the special SEO title for each major page.

--- pages/about.vue
+++ pages/about.vue
@@ -2,11 +2,22 @@
   <AboutMe />
 </template>
 
-<script lang="ts">
+<script>
 import AboutMe from '@/components/AboutMe.vue'
+import { seo } from '@/static/data/seo'
 
 export default {
   components: { AboutMe },
   layout: 'default',
+  data() {
+    return {
+      seo,
+    }
+  },
+  head() {
+    return {
+      title: seo.aboutMe.title,
+    }
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,22 @@
   <Main />
 </template>
 
-<script lang="ts">
+<script>
 import Vue from 'vue'
-import Main from '~/components/Main.vue'
+import Main from '@/components/Main.vue'
+import { seo } from '@/static/data/seo'
 
 export default Vue.extend({
   components: { Main },
+  data() {
+    return {
+      seo,
+    }
+  },
+  head() {
+    return {
+      title: seo.home.title,
+    }
+  },
 })
 </script>
