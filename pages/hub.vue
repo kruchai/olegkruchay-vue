40142bf94ce61e9fc1c373515f20895c848caf64
Oleg Kruchay <okruchay@gmail.com>
2020-12-10 22:19:19 +0200

    v0.7.9: Adding redirect for hub service.

---
+++ pages/hub.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ seo.hub.title }}</h1>
+</template>
+
+<script>
+import { seo } from '~/static/data/seo'
+
+export default {
+  layout: 'default',
+  data() {
+    return {
+      seo,
+    }
+  },
+  head() {
+    return {
+      title: seo.hub.title,
+    }
+  },
+  middleware({ redirect }) {
+    return redirect('https://hub.olegkruchay.com')
+  },
+}
+</script>
