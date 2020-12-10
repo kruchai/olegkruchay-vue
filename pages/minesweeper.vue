3b113d03c41d44aac68476f305bcc0fc0ad2804d
Oleg Kruchay <okruchay@gmail.com>
2020-12-10 22:19:45 +0200

    v0.7.9: Adding redirect for the minesweeper game.

---
+++ pages/minesweeper.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>{{ seo.minesweeper.title }}</h1>
+</template>
+
+<script>
+import { seo } from '~/static/data/seo'
+
+export default {
+  layout: 'default',
+  data() {
+    return {
+      seo,
+    }
+  },
+  head() {
+    return {
+      title: seo.minesweeper.title,
+    }
+  },
+  middleware({ redirect }) {
+    return redirect('https://hub.olegkruchay.com/minesweeper-vue')
+  },
+}
+</script>
