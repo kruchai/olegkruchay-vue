ae1b6c4674db16c5e3a44f871e301f4a2037213c
Oleg Kruchay <okruchay@gmail.com>
2021-06-10 17:22:43 +0300

    v0.9: Preparing to integration with G Suite. Adding the pages which are redirects to API data.

---
+++ pages/info.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ seo.data.title }}</h1>
+</template>
+
+<script>
+import { seo } from '~/static/data/seo'
+
+export default {
+  layout: 'default',
+  data() {
+    return {
+      seo,
+    }
+  },
+  head() {
+    return {
+      title: seo.data.title,
+    }
+  },
+  middleware({ redirect }) {
+    return redirect(
+      'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ3ERZTpDKbzjUqtBZIeUwkg665EwVvuk9DoE0AEdN8fFQEvHCmdm-M0e6Yo8rrj6gjKGqYbRYIPaNn/pubhtml'
+    )
+  },
+}
+</script>
